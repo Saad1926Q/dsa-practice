b051912934cdda0f37eda398277ff113f1e8ac8e
Saad1926Q <saad.ahmed1926q@gmail.com>
2024-12-08 18:46:12 +0530

    valid anagram

---
+++ Problem-Solving/LeetCode/LC_242/lc_242.js
@@ -0,0 +1,57 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+//Given two strings s and t, return true if t is an anagramof s, and false otherwise.
+
+/*
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+*/
+
+s="hello"
+t="oleh"
+
+var isAnagram = function(s, t) {
+    hashmap1=new Map()
+    hashmap2=new Map()
+
+    result=true
+
+    for(char of s){
+        if(hashmap1.has(char)){
+            prev_freq=hashmap1.get(char)
+            hashmap1.set(char,prev_freq+1)
+        }else{
+            hashmap1.set(char,1)
+        }
+    }
+
+    for(char of t){
+        if(!hashmap1.has(char)){
+            return false
+        }else if(hashmap2.has(char)){
+            prev_freq=hashmap2.get(char)
+            hashmap2.set(char,prev_freq+1)
+        }else{
+            hashmap2.set(char,1)
+        }
+
+    }
+
+    console.log(hashmap1)
+    console.log(hashmap2)
+
+    hashmap1.forEach((freq,char) => {
+        if(hashmap2.get(char)!==freq){
+            result=false;
+        }
+    });
+
+    return result;
+};
+
+console.log(isAnagram(s,t))
